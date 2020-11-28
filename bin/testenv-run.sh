7f738ec12856edc249986955b7006ab9082f7273
Matteo Agostini <matteo.agostini@ph.tum.de>
2020-11-28 16:24:53 +0100

    tmp copy of load

--- bin/testenv-run.sh
+++ bin/testenv-run.sh
@@ -1,6 +1,57 @@
-echo This will run the production in future
+#!/bin/bash
 
-# Arguments:
-# - production_tag or so
+###############################################################################
+# Description
+###############################################################################
 
-# Should source testenv-load and run testenv-test automatically first
+usage() { 
+\echo >&2  "Usage: testenv-load [OPTIONS] ./path/to/config.json"
+\cat >&2 <<EOF
+
+This script load the software of a production cycle and loads it
+
+Options:
+   -?    ????
+EOF
+exit 1;
+}
+
+###############################################################################
+# Check whether the setup.sh has been already sourced
+###############################################################################
+if [ -z "$TESTENV_REFPROD" ] || [ -z "$TESTENV_USERPROD" ]; then
+   \echo "Error: source setup.sh before continuing.";
+   exit 1;
+fi
+
+###############################################################################
+# Set defaults, parse options, performs checks
+###############################################################################
+testenv-load() {
+
+# Check mandatory arguments
+if [ -z "$1" ]; then
+   usage
+fi
+# Check mandatory arguments
+if [ ! -f "$1" ]; then
+   \echo "Error: config file not valid."
+   exit 1;
+fi
+
+local INST=`\python -c "\
+import sys, json, os;
+config_file = sys.argv[1];
+config_file_dir = os.path.dirname(os.path.abspath(config_file));
+config_dic = json.load(open(config_file));
+target = config_dic['setups']['testenv']['software']['inst'];
+print(os.path.join(config_file_dir,target));
+" $1`
+
+export PYTHONPATH=$INST
+export PYTHONUSERBASE=$INST
+
+echo "Done."
+}
+
+testenv-load "$@"
