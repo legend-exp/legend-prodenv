1da8c7933e646c2a1e9eb27bbf3966f88fcebef4
Matteo Agostini <matteo.agostini@ph.tum.de>
2020-11-28 15:31:36 +0100

    first working version of the build script

--- bin/readconfig.py
+++
@@ -1,6 +0,0 @@
-# To be written
-
-# Call as
-# readconfig.py PROD_TAG setups/legend200/src/python
-# readconfig.py PROD_TAG setups/legend200/inst/python
-# ...
--- bin/testenv-build.sh
+++ bin/testenv-build.sh
@@ -1,6 +1,47 @@
-echo This script should install the sofware
+#!/bin/bash
 
-# Arguments:
-# - production_tag or so
+###############################################################################
+# Description
+###############################################################################
 
-# Will run pip install, etc.
+usage() { 
+\echo >&2  "Usage: testenv-build [OPTIONS] ./path/to/config.json"
+\cat >&2 <<EOF
+
+This script install the software in the production cycle
+
+Options:
+   -?    ????
+EOF
+exit 1;
+}
+
+###############################################################################
+# Actual script implemented as function to protect against users sourcing it
+###############################################################################
+testenv-build() {
+
+local SRC=`\python -c "\
+import sys, json, os;
+config_file = sys.argv[1];
+config_file_dir = os.path.dirname(os.path.abspath(config_file));
+config_dic = json.load(open(config_file));
+target = config_dic['setups']['testenv']['software']['src'];
+print(os.path.join(config_file_dir,target));
+" $1`
+
+local INST=`\python -c "\
+import sys, json, os;
+config_file = sys.argv[1];
+config_file_dir = os.path.dirname(os.path.abspath(config_file));
+config_dic = json.load(open(config_file));
+target = config_dic['setups']['testenv']['software']['inst'];
+print(os.path.join(config_file_dir,target));
+" $1`
+
+export PYTHONPATH=$INST
+export PYTHONUSERBASE=$INST
+\python -m pip install -e $SRC/pygama
+}
+
+testenv-build "$@"
