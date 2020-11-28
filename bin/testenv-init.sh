6d87ced62aced531d45498360eb9a287ad3203f3
Matteo Agostini <matteo.agostini@ph.tum.de>
2020-11-28 10:50:11 +0100

    cleaned up script. Debugged and complete version

--- bin/testenv-init.sh
+++ bin/testenv-init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+###############################################################################
 # Description
+###############################################################################
+
 usage() { 
 \echo >&2  "Usage: testenv-init [OPTIONS] PRODUCTION_TAG"
 \cat >&2 <<EOF
@@ -9,43 +12,46 @@ This script initializes a new production cycle named PRODUCTION_TAG.
 PRODUCTION_TAG is a mandatory argument and should be parsed after the options. 
 
 Options:
-   -p    set custom path to pygamma source directory. If not specificied, 
-         pygamma is downloaded and stored in the production cycle structure
+   -p    custom path to pygamma source directory. Install pygamma if not found
+         [default: new-production-cycle/software/src]
 
-   -u    set the url from which to clone pygamma. By default it is 
-         git@github.com:legend-exp/pygama.git  
+   -o    set github organization pygamma is cloned from [default: legend-exp]
 
-   -b    set which branch to checkout. The default is the master
+   -b    set which branch to checkout [default: master]
 
-   -r    create a reference production cycle. By default a user production cycle
-         is created
+   -r    create a reference production cycle. By default the production cycle
+         is created in the user space
 EOF
 exit 1;
 }
 
-# Check that setup.sh has been already sourced
+###############################################################################
+# Set defaults, parse options, performs checks
+###############################################################################
+
+# Set defaults
+PYGAMMA_ORGANIZATION="legend-exp"
+PYGAMMA_BRANCH="master"
+PYGAMMA_PATH=""
+PROD_ENV=${TESTENV_USERPROD}
+
+# Check whether the setup.sh has been already sourced
 if [ -z "$TESTENV_REFPROD" ] || [ -z "$TESTENV_USERPROD" ]; then
    \echo "Error: source setup.sh before continuing.";
    exit 1;
 fi
 
-# Parse options
+# Parse options and overwrite the variable default value
 while getopts "p:u:b:rh:" options; do
    case ${options} in
-      p) PYGAMMASRC=${OPTARG};;
-      u) PYGAMMAURL=${OPTARG};;
-      b) PYGAMMABRC=${OPTARG};;
+      p) PYGAMMA_PATH=${OPTARG};;
+      u) PYGAMMA_ORGANIZATION=${OPTARG};;
+      b) PYGAMMA_BRANCH=${OPTARG};;
       r) PROD_ENV=$TESTENV_REFPROD;;
       h) usage;;
    esac
 done
 shift $((OPTIND - 1))
-
-
-# Set user prod env unless -r option is used
-if [ -z "${PROD_ENV}" ]; then
-    PROD_ENV=$TESTENV_USERPROD
-fi
 
 # Get and test production tag
 PRODUCTION_TAG="$1"
@@ -53,18 +59,22 @@ if [ -z "${PRODUCTION_TAG}" ]; then
     usage
 fi
 
-# check that the production cycle does not exist
-if [ -d "$PROD_ENV/$PRODUCTION_TAG/" ]; then
+# Check whether the production cycle already exists
+if [ -d "$PROD_ENV/$PRODUCTION_TAG" ]; then
    \echo "Error: a data production cycle with the same name already exists"
    exit 1;
 fi
+
+###############################################################################
+# Build new production enviroment
+###############################################################################
 
 # Create file system
 \mkdir -p $PROD_ENV/$PRODUCTION_TAG/software/{inst,src}
 \mkdir -p $PROD_ENV/$PRODUCTION_TAG/data/{gen,meta}
 
 # Create json config file
-\cat > ${PROD_ENV}/${PRODUCTION_TAG}/dataflow-config.json  <<EOF
+\cat > ${PROD_ENV}/${PRODUCTION_TAG}/config.json  <<EOF
 {
   "setups": {
     "routine-dev": {
@@ -82,21 +92,17 @@ fi
 } 
 EOF
 
-# Replace (FIXME: Oli, perhaps you find a way to do this in a nicer way)
+# Replace raw data path based on REFPROD name
+# FIXME: this is pretty ugly hack
 TESTENV_REFPROD_BASENAME=`\basename $TESTENV_REFPROD`
 \sed -i "s/TESTENV_REFPROD_BASENAME/${TESTENV_REFPROD_BASENAME}/g" ${PROD_ENV}/${PRODUCTION_TAG}/dataflow-config.json
 
 # Install pygramma if path is empty
-if [ -z "${PYGAMMASRC}" ]; then
-   # Set default url if empty
-   if [ -z "${PYGAMMAURL}" ]; then
-      PYGAMMAURL="git@github.com:legend-exp/pygama.git"
-   fi
-   # Set default branch if empty
-   if [ -z "${PYGAMMABRC}" ]; then
-      PYGAMMABRC="master"
-   fi
-   \git clone ${PYGAMMAURL} ${PROD_ENV}/${PRODUCTION_TAG}/software/src/pygamma --branch ${PYGAMMABRC}
+if [ -z "${PYGAMMA_PATH}" ]; then
+   \git clone \
+      git@github.com:${PYGAMMA_ORGANIZATION}/pygama.git \
+      ${PROD_ENV}/${PRODUCTION_TAG}/software/src/pygamma \
+      --branch ${PYGAMMA_BRANCH}
 fi
 
 echo "Done."
