ac56a398117edd84dd24c3154d159c6b2c4382b9
Matteo Agostini <matteo.agostini@ph.tum.de>
2021-03-26 13:56:00 +0100

    updated init script

--- bin/prodenv-init-cycle.sh
+++ bin/prodenv-init-cycle.sh
@@ -11,18 +11,23 @@ This script initializes a new production cycle named PRODUCTION_TAG.
 PRODUCTION_TAG is a mandatory argument and should be parsed after the options. 
 
 Options:
-   -p <path>   Set custom path to user source directory. When not specified, 
-               all user software is cloned into new-production-cycle/src/python
+   -p <path>   Set custom path to user src directory. When not specified, 
+               all user software is cloned from github
+               [default path: new-production-cycle/src/python]
 
-   -o <name>   Set the name of the github organization software is cloned from
-               [default: legend-exp]
+   -o <name>   Set the name of the github organization software is cloned from.
+               Can be used to clone the user's forks 
+               [default organization: $GITHUB_ORGANIZATION]
 
-   -b <name>   Set name of the branch to check out   [default: master]
+   -b <name>   Set name of the branch to check out
                Note that all reps need to have a branch with the same name
+               [default name: $GITHUB_BRANCH]
 
-   -c <path>   Set path to singularity container defaultp
+   -c <path>   Set path to singularity container
+               [default path: $PRODENV_DEFAULT_CONTAINER]
 
-   -r          Create a production cycle under prod-ref [default: prod-user]
+   -r          Create a production cycle under prod-ref
+               [default: prod-user]
 
 EOF
 exit 1;
@@ -50,6 +55,7 @@ local GITHUB_ORGANIZATION="legend-exp"
 local GITHUB_BRANCH="master"
 local PATH_SRC=""
 local PATH_CYCLE=$PRODENV_PROD_USER
+local PATH_CONTAINER=$PRODENV_DEFAULT_CONTAINER
 
 # Parse options and overwrite the variable default value
 while getopts "p:o:b:rh:" options; do
@@ -57,6 +63,7 @@ while getopts "p:o:b:rh:" options; do
       p) PATH_SRC=${OPTARG};;
       o) GITHUB_ORGANIZATION=${OPTARG};;
       b) GITHUB_BRANCH=${OPTARG};;
+      c) PATH_CONTAINER=${OPTARG};;
       r) PATH_CYCLE=$PRODENV_PROD_REF;;
       h) usage;;
    esac
@@ -95,6 +102,7 @@ fi
 \mkdir -p $PATH_CYCLE/$PRODUCTION_TAG/src/python
 
 if [ -z "$PATH_SRC" ]; then
+   PATH_SRC="\$_/src/python"
    \git clone \
       git@github.com:$GITHUB_ORGANIZATION/pygama.git \
       $PATH_CYCLE/$PRODUCTION_TAG/src/python/pygama \
@@ -108,44 +116,29 @@ fi
 # venv: venv local files
 \mkdir -p $PATH_CYCLE/$PRODUCTION_TAG/venv/default/user
 #FIXME
-#\cp -l  $PRODENV_DEFAULT_CONTAINER $PATH_CYCLE/$PRODUCTION_TAG/venv/default/
-ln -s  $PRODENV_DEFAULT_CONTAINER $PATH_CYCLE/$PRODUCTION_TAG/venv/default/
-\ln -s `\basename $PRODENV_DEFAULT_CONTAINER` \
-       $PATH_CYCLE/$PRODUCTION_TAG/venv/default/rootfs.sif
-
-# FIXME:clean this
-# tools: software tools
-#\mkdir -p $PATH_CYCLE/$PRODUCTION_TAG/tools
-#\git clone \
-#   git@github.com:oschulz/singularity-venv.git \
-#   $PATH_CYCLE/$PRODUCTION_TAG/tools/venv
-
-# FIXME: clean this up
-# bin: executable and scripts
-#\mkdir -p $PATH_CYCLE/$PRODUCTION_TAG/bin
-#\ln -s ../tools/venv/bin/venv $PATH_CYCLE/$PRODUCTION_TAG/bin/
-
+#\cp -l  $PATH_CONTAINER $PATH_CYCLE/$PRODUCTION_TAG/venv/default/
+\ln -s  $PATH_CONTAINER $PATH_CYCLE/$PRODUCTION_TAG/venv/default/
+\ln -s `\basename $PATH_CONTAINER` $PATH_CYCLE/$PRODUCTION_TAG/venv/default/rootfs.sif
 
 # Create json config file
-ROOTDIR=$PATH_CYCLE/$PRODUCTION_TAG
 \cat > $PATH_CYCLE/$PRODUCTION_TAG/config.json  <<EOF
 {
     "setups": {
         "l200hades": {
             "paths": {
                 "orig": "/lfs/l1/legend/detector_char/enr/hades/char_data",
-                "gen": "$ROOTDIR/gen",
-                "meta": "$ROOTDIR/meta",
-                "par": "$ROOTDIR/par",
+                "gen": "\$_/gen",
+                "meta": "\$_/meta",
+                "par": "\$_/par",
                 "src": {
-                    "python": "$ROOTDIR/src/python"
+                    "python": "$PATH_SRC"
                 }
             },
             "execenv": {
                 "envvars": {
-                    "VENV_BASE_DIR": "$ROOTDIR/venv"
+                    "VENV_BASE_DIR": "\$_/venv"
                 },
-                "cmd": ["$ROOTDIR/tools/venv/bin/venv", "legend"]
+                "cmd": ["\$_/tools/venv/bin/venv", "default"]
             }
         }
     }
