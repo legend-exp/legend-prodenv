ec20be3b2dfa05d1ffb5dbc6650b19b45bde7949
Matteo Agostini <matteo.agostini@ph.tum.de>
2022-06-22 11:56:01 +0100

    removed deprecated options

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
 # Main setup file for the production enviroment. It needs to be sourced
 export PRODENV="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH="${PRODENV}/bin:${PRODENV}/tools/bin:$PATH"
-
-# site specific paths
-export PRODENV_DEFAULT_CONTAINER='/lfs/l1/legend/software/singularity/legendexp_legend-base_latest_20210313112951.sif'
-export PRODENV_DEFAULT_ORIG='/lfs/l1/legend/detector_char/enr/hades/char_data'
-
