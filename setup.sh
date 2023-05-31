746fe0159fb6f54e193ac98494807b18dff97664
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-05-31 12:26:54 +0200

    Make setup.sh portable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Main setup file for the production enviroment. It needs to be sourced
-export PRODENV="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PRODENV=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 export PATH="${PRODENV}/bin:${PRODENV}/tools/bin:$PATH"
 export PYTHONDONTWRITEBYTECODE=1
