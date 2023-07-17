9330b47da6f0909038a3704e5fdd0dde8a75a5a8
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-07-17 13:12:37 +0200

    Use python3 in scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PRODENV=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+# custom prodenv software gets precedence
 PATH="${PRODENV}/bin:${PRODENV}/tools/bin:$PATH"
 PYTHONDONTWRITEBYTECODE=1
 
